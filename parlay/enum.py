8d949437fdedd76f4f7f20a995d4ba6576fd098c
Daniel Beard <daniel@promenadesoftware.com>
2015-05-15 15:38:43 -0700

    Added SSCOM protocol to parlay, with simple tests

--- parlay/enum.py
+++ parlay/enum.py
@@ -1,3 +1,7 @@
+
+
+
+#todo: space underscore mapping
 def enum(*sequential, **named):
     enums = dict(zip(sequential, range(len(sequential))), **named)
     reverse = dict((value, key) for key, value in enums.iteritems())
