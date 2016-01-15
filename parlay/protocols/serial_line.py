8d1627347fc9660e996b9f785ec9c8e4ff2c56fa
Jeff Gable <jeff@promenadesoftware.com>
2016-01-15 14:19:12 -0800

    serial_line.py: fix bugs and formatting issues in ASCIILineProtocol

--- parlay/protocols/serial_line.py
+++ parlay/protocols/serial_line.py
@@ -3,8 +3,9 @@ from parlay.server.broker import Broker
 from twisted.internet import defer
 from twisted.internet.serialport import SerialPort
 from twisted.protocols.basic import LineReceiver
-from parlay.endpoints.parlay_standard import ParlayCommandEndpoint, parlay_command, MSG_TYPES, BadStatusError
-from parlay.protocols.utils import delay, timeout
+from parlay.endpoints.parlay_standard import ParlayCommandEndpoint, parlay_command, MSG_TYPES
+from parlay.protocols.utils import timeout
+
 
 class ASCIILineProtocol(BaseProtocol, LineReceiver):
     """
@@ -20,44 +21,48 @@ class ASCIILineProtocol(BaseProtocol, LineReceiver):
         This will be called bvy the system to construct and open a new SSCOM_Serial protocol
         :param cls : The class object (supplied by system)
         :param broker:  current broker insatnce (supplied by system)
-        :param port: the serial port device to use. On linux, something like/dev/ttyUSB0 on windows something like COM0
+        :param port: the serial port device to use. On linux, something like "/dev/ttyUSB0". On windows something like "COM0"
+        :param baudrate: baudrate of serial connection
+        :param delimiter:
         """
         if isinstance(port, list):
             port = port[0]
 
         p = ASCIILineProtocol(port)
-        SerialPort(p, port, broker._reactor, baudrate=115200)  # int(baudrate))
+        cls.delimiter = str(delimiter).decode("string_escape")
+        SerialPort(p, port, broker._reactor, baudrate=baudrate)
 
         return p
 
     @classmethod
     def get_open_params_defaults(cls):
-        """Override the default implementation of 'get default open param' to return a list"""
+        """Override base class function to show dropdowns for defaults"""
         from serial.tools import list_ports
-        # default impl
+
         defaults = BaseProtocol.get_open_params_defaults()
 
         potential_serials = [x[0] for x in list_ports.comports()]
         defaults['port'] = potential_serials
-        defaults['baudrate'] = [9600, 57600]
+        defaults['baudrate'] = [300, 1200, 2400, 4800, 9600, 14400, 19200, 28800, 38400, 57600, 115200, 230400]
         defaults['delimiter'] = "\n"
 
         return defaults
 
     def close(self):
         self.transport.loseConnection()
-        return defer.succeed(None)  # fake deferred since we don't have anything asynchornous to do
+        return defer.succeed(None)  # fake deferred since we don't have anything asynchronous to do
 
     def __init__(self, port):
-        BaseProtocol.__init__(self)
         self._parlay_name = port
-        self.endpoints = [LineEndpoint(self._parlay_name, self._parlay_name, self)]
+        self.endpoints = getattr(self, "endpoints", [LineEndpoint(self._parlay_name, self._parlay_name, self)])
+        BaseProtocol.__init__(self)
 
     def lineReceived(self, line):
         # only 1 endpoint
         self.endpoints[0].send_message(to="UI", contents={"DATA": line}, msg_type=MSG_TYPES.EVENT)
 
-
+    def rawDataReceived(self, data):
+        pass
 
     def __str__(self):
         return "Serial Terminal @ " + self._parlay_name
@@ -80,4 +85,3 @@ class LineEndpoint(ParlayCommandEndpoint):
             if next_resp["TOPICS"].get("MSG_TYPE", "") == MSG_TYPES.EVENT:
                     resp = next_resp["CONTENTS"]["DATA"]
                     defer.returnValue(resp)
-
