6d50dc0d2b6d65c8269ad4cd69162799e75e5e65
Daniel Beard <daniel@promenadesoftware.com>
2016-08-23 18:47:50 -0700

    Added send_and_wait_for_data to Serial LineItem

--- parlay/protocols/serial_line.py
+++ parlay/protocols/serial_line.py
@@ -85,6 +85,17 @@ class LineItem(ParlayCommandItem):
         self._protocol.sendLine(str(data))
 
     @parlay_command(async=True)
+    def send_and_wait_for_data(self, data, timeout_secs):
+        """
+        Send raw Data and then wait for the response
+        :param data:
+        :return:
+        """
+        handler = self._protocol.get_new_data_wait_handler()
+        self._protocol.sendLine(str(data))
+        return handler.wait(timeout_secs)
+
+    @parlay_command(async=True)
     def wait_for_data(self, timeout_secs=3):
         """
         :type timeout_secs float
