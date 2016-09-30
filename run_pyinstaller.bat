61a85a6f41ddf1c93873345edbbe17130e94291c
Daniel Beard <daniel@promenadesoftware.com>
2016-09-29 19:27:00 -0700

    Added windows specific options to parlay pyinstaller script

--- run_pyinstaller.bat
+++ run_pyinstaller.bat
@@ -3,9 +3,10 @@
 #install requirements
 pip install pyinstaller
 pip install -r requirements.txt
-
+# there is a bug with pyinstaller and setuptools after 19.2 on windows
+pip install setuptools==19.2
 # run installer
-pyinstaller --onefile --hidden-import=queue  run_parlay.py
+pyinstaller --uac-admin --onefile --icon parlay.ico --hidden-import=queue  run_parlay.py
 
 
 #copy the ui
