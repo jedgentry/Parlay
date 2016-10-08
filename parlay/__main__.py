49920731479f38bd8136739333d265d7d59ee639
Daniel Beard <daniel@promenadesoftware.com>
2016-10-07 18:29:41 -0700

    Added serial import to __main__.py

--- parlay/__main__.py
+++ parlay/__main__.py
@@ -2,7 +2,7 @@
 # This file is a standard entry point for parlay from the command line
 from parlay import start
 from protocols.pcom import pcom_serial
-
+from protocols import serial_line
 
 def main():
     start()
