79541669eb94989f9e4877ca6c4085081f40fb6b
Jed Gentry <jedgentry@mac.com>
2017-10-13 13:49:21 -0700

    Scripting: Added ability for scripts to publish messages. (#38)

--- parlay/utils/__init__.py
+++ parlay/utils/__init__.py
@@ -5,6 +5,7 @@ import scripting_setup
 setup = scripting_setup.setup
 
 subscribe = lambda fn, **kwargs: scripting_setup.script.subscribe(fn, **kwargs)
+publish = lambda msg: scripting_setup.script.publish(msg)
 discover = lambda force=True: scripting_setup.script.discover(force)
 get_item_by_name = lambda item_name: scripting_setup.script.get_item_by_name(item_name)
 get_item_by_id = lambda item_id: scripting_setup.script.get_item_by_id(item_id)
